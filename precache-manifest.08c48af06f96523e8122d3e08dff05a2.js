d45d0c2b2d73b1ae5154d6e68d265cc8bfa1154b
Michelle Morin <morin2m@gmail.com>
2020-05-01 16:46:22 -0700

    Updates

---
+++ precache-manifest.08c48af06f96523e8122d3e08dff05a2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8383fd5f4617c1716ca68562eb73f2c",
+    "url": "/redux-taproom/index.html"
+  },
+  {
+    "revision": "c958eb3d67250b768172",
+    "url": "/redux-taproom/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "76006a1ad433cfc9198a",
+    "url": "/redux-taproom/static/css/main.d8099662.chunk.css"
+  },
+  {
+    "revision": "c958eb3d67250b768172",
+    "url": "/redux-taproom/static/js/2.7527f1d4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/redux-taproom/static/js/2.7527f1d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76006a1ad433cfc9198a",
+    "url": "/redux-taproom/static/js/main.67ff4a7a.chunk.js"
+  },
+  {
+    "revision": "79353f6623dc7a3e846b",
+    "url": "/redux-taproom/static/js/runtime-main.267e2067.js"
+  },
+  {
+    "revision": "035cf0e79966647deefa61be1fe99e04",
+    "url": "/redux-taproom/static/media/beer.035cf0e7.png"
+  },
+  {
+    "revision": "2ba599b9e0cc81f19fd2ef035800c43c",
+    "url": "/redux-taproom/static/media/brushstroke.2ba599b9.png"
+  },
+  {
+    "revision": "827a1fb880c1f72d6e4a536bf9c8609f",
+    "url": "/redux-taproom/static/media/hops.827a1fb8.jpg"
+  }
+]);
