333e285a85f6b584c09ce6176d5ecc4bd9c4cb41
Michelle Morin <morin2m@gmail.com>
2020-05-01 08:32:45 -0700

    add ADD_KEG case to reducer to pass second test

--- src/reducers/tap-list-reducer.js
+++ src/reducers/tap-list-reducer.js
@@ -1,3 +1,18 @@
 export default (state = {}, action) => {
-  return state;
+  const { name, brewery, alcoholContent, ibu, price, id } = action;
+  switch(action.type) {
+    case 'ADD_KEG':
+      return Object.assign({}, state, {
+        [id] : {
+          name: name,
+          brewery: brewery,
+          alcoholContent: alcoholContent,
+          ibu: ibu,
+          price: price,
+          id: id
+        }
+      });
+    default:
+      return state;
+  }
 };
