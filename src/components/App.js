578e11c8dc557bd979e269a720663e9e32a07ae2
Michelle Morin <morin2m@gmail.com>
2020-05-01 15:19:43 -0700

    move className into TapControl component instead of separate div

--- src/components/App.js
+++ src/components/App.js
@@ -11,9 +11,7 @@ function App() {
   return (
     <div style={appStyles}>
       <Header />
-      <div className="tap-control">
-        <TapControl />
-      </div>
+      <TapControl className="tap-control" />
       <Footer />
     </div>
   );
