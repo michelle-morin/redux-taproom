e9403e04ee9a21192b4005d63f97e00da43a0051
Michelle Morin <morin2m@gmail.com>
2020-05-01 08:57:20 -0700

    add second test for changing selected keg, receive meaningful fail

--- src/__tests__/reducers/selected-keg-reducer.test.js
+++ src/__tests__/reducers/selected-keg-reducer.test.js
@@ -5,4 +5,25 @@ describe('selectedKegReducer', () => {
     expect(selectedKegReducer(null, { type: null })).toEqual(null);
   });
 
+  test('should successfully set the selectedKeg to the keg passed to the reducer', () => {
+    const action = {
+      type: 'CHANGE_SELECTED',
+      name: 'hazy IPA',
+      brewery: 'ex novo',
+      alcoholContent: 7,
+      ibu: 55,
+      price: 7,
+      id: 1
+    };
+    expect(selectedKegReducer(null, action)).toEqual({
+      [1]: {
+        name: 'hazy IPA',
+        brewery: 'ex novo',
+        alcoholContent: 7,
+        ibu: 55,
+        price: 7,
+        id: 1 
+      }
+    });
+  });
 });
