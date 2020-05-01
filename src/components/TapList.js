4bca485c5afac6c2c152a797df782c9034d08831
Michelle Morin <morin2m@gmail.com>
2020-05-01 11:22:01 -0700

    sort display of kegs by date on which kegs were tapped, ordered from oldest keg to newest keg

--- src/components/TapList.js
+++ src/components/TapList.js
@@ -13,19 +13,23 @@ function TapList(props){
 
   return(
     <div style={tapListStyles}>
-      {Object.values(props.tapList).map((keg) =>
-      <Keg 
-        whenBuyClicked = {props.onClickingBuy}
-        whenKegClicked = {props.onKegSelection}
-        name={keg.name}
-        brewery={keg.brewery}
-        alcoholContent={keg.alcoholContent}
-        ibu={keg.ibu}
-        price={keg.price}
-        pintQuantity={keg.pintQuantity}
-        id={keg.id}
-        key={keg.id} />
-      )}
+      {Object.values(props.tapList).sort((a,b) => {
+        return (a.kegTappedDate - b.kegTappedDate);
+      }).map((keg) => {
+        return (
+          <Keg whenBuyClicked = {props.onClickingBuy}
+            whenKegClicked = {props.onKegSelection}
+            name={keg.name}
+            brewery={keg.brewery}
+            alcoholContent={keg.alcoholContent}
+            ibu={keg.ibu}
+            price={keg.price}
+            pintQuantity={keg.pintQuantity}
+            kegTappedDate={keg.kegTappedDate}
+            id={keg.id}
+            key={keg.id} />
+        );
+      })}
     </div>
   );
 }
