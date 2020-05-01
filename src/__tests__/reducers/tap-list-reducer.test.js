8c92af3e0e35537eb5ea9017abcc27365c96098f
Michelle Morin <morin2m@gmail.com>
2020-05-01 08:30:12 -0700

    create second test for taplistreducer, meaningful fail

--- src/__tests__/reducers/tap-list-reducer.test.js
+++ src/__tests__/reducers/tap-list-reducer.test.js
@@ -1,8 +1,41 @@
 import tapListReducer from '../../reducers/tap-list-reducer';
+import { v4 } from 'uuid';
 
 describe('tapListReducer', () => {
 
+  const newKeg = {
+    name: 'hazy IPA',
+    brewery: 'ex novo',
+    alcoholContent: 7,
+    ibu: 55,
+    price: 7,
+    id: v4()
+  }
+
   test('should return default state if there is no action type passed into the reducer', () => {
     expect(tapListReducer({}, { type: null })).toEqual({});
+  });
+
+  test('should successfully add new keg to masterKegList', () => {
+    const { name, brewery, alcoholContent, ibu, price, id } = newKeg;
+    const action = {
+      type: 'ADD_KEG',
+      name: name,
+      brewery: brewery,
+      alcoholContent: alcoholContent,
+      ibu: ibu,
+      price: price,
+      id: id
+    };
+    expect(tapListReducer({}, action)).toEqual({
+      [id] : {
+        name: name,
+        brewery: brewery,
+        alcoholContent: alcoholContent,
+        ibu: ibu,
+        price: price,
+        id: id
+      }
+    });
   });
 });
