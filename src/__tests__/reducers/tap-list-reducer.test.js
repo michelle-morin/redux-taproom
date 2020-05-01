21261eb4fcbaea7a8f3075a36900052b6e2f1fe1
Michelle Morin <morin2m@gmail.com>
2020-05-01 08:36:57 -0700

    write test for deleting keg, receive meaningful test fail

--- src/__tests__/reducers/tap-list-reducer.test.js
+++ src/__tests__/reducers/tap-list-reducer.test.js
@@ -3,14 +3,36 @@ import { v4 } from 'uuid';
 
 describe('tapListReducer', () => {
 
+  const id1 = v4();
+  const id2 = v4();
+
   const newKeg = {
     name: 'hazy IPA',
     brewery: 'ex novo',
     alcoholContent: 7,
     ibu: 55,
     price: 7,
-    id: v4()
-  }
+    id: id1
+  };
+
+  const currentKegList = {
+    id1: {
+      name: 'hazy IPA',
+      brewery: 'ex novo',
+      alcoholContent: 7,
+      ibu: 55,
+      price: 7,
+      id: id1
+    },
+    id2: {
+      name: 'cucumber crush',
+      brewery: '10 barrel brewing co.',
+      alcoholContent: 5,
+      ibu: 4,
+      price: 6,
+      id: id2
+    }
+  };
 
   test('should return default state if there is no action type passed into the reducer', () => {
     expect(tapListReducer({}, { type: null })).toEqual({});
@@ -35,6 +57,23 @@ describe('tapListReducer', () => {
         ibu: ibu,
         price: price,
         id: id
+      }
+    });
+  });
+
+  test('should successfully delete a keg', () => {
+    const action = {
+      type: 'DELETE_KEG',
+      id: 1
+    };
+    expect(tapListReducer(currentKegList, action)).toEqual({
+      id2: {
+        name: 'cucumber crush',
+        brewery: '10 barrel brewing co.',
+        alcoholContent: 5,
+        ibu: 4,
+        price: 6,
+        id: id2
       }
     });
   });
